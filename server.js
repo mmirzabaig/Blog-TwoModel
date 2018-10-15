259f51001052df31d2ab94408b95466f92589b5f
Mirza Baig <mirzabaig@Mirzas-MacBook-Pro.local>
2018-10-15 10:46:29 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const add = express();
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
