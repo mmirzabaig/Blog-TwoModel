e4d3c26d51d32177b8f980eff64565f2c6fed5d2
Mirza Baig <mirzabaig@Mirzas-MacBook-Pro.local>
2018-10-15 10:53:55 -0500

    server is working

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
-const mongoose = require('mongoose');
 const express = require('express');
-const add = express();
+const app = express();
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+
+app.get('/', (req, res) => {
+  res.send('This is your blog app')
+});
+
+
+
+
+app.listen(3000, () => {
+  console.log('Your App is LIVE!');
+});
